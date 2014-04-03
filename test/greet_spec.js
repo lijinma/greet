3879e470c5414f87f8e50b2ec9f0fb850fc519a7
Jinma li <lijinma@126.com>
2014-04-04 01:14:22 +0800

    - add test

---
+++ test/greet_spec.js
@@ -0,0 +1,13 @@
+var expect = require("chai").expect
+var greet = require('greet');
+
+describe('greet', function(){
+  it('should greet a person by name', function() {
+    var result = greet('name');
+    expect(result).to.equal('hello, name');
+  });
+  it('should greet a person flirtatiously if drunk', function() {
+    var result = greet('name', 'drunk')
+    expect(result).to.equal('hello name, you look sexy today');
+  });
+});
